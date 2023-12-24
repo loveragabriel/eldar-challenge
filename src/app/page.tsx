918952d25fa987feef04258109a5d41d883d6985
Gabriel <loveragabriel20@gmail.com>
2023-12-24 08:45:23 -0300

    corrijo acceso boton de ingreso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
-import { Container, Typography, Button} from '@mui/material'
+import { Container, Typography, Button } from '@mui/material'
 
 export default function Home() {
   return (
-    <Container sx={{height:'100vh',}}>
-      <Typography variant='h4' component="h1" sx={{fontSize: {
-        xs:'1.5rem', 
-        sm:'2.5rem'
-      }}} >Front End Challenge </Typography >
+    <Container sx={{ height: '100vh', }}>
+      <Typography variant='h4' component="h1" sx={{
+        fontSize: {
+          xs: '1.5rem',
+          sm: '2.5rem'
+        }
+      }} >Front End Challenge </Typography >
       <Typography variant='h6'>Gabriel Lovera</Typography>
-      <Button variant='contained' sx={{padding:'10px'}} >
-        <Link href={'/log-in'}>Ingresar
-        </Link>
+      <Link href={'/log-in'}>
+        <Button variant='contained' sx={{ padding: '10px' }} >
+          Ingresar
         </Button>
+      </Link>
     </Container>
   )
 }
